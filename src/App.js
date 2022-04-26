0e441353f8b3d2dbc5caf9e8c060672e03ba2288
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-04-26 20:18:08 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 /** Import de la donnée */
-import './App.css';
+import "./App.css";
 
-import Robot from './data/data';
+import Robot from "./data/data";
 
 function App() {
-  console.log(Robot)
-  return (
-    <div className="App">
-      <h1>Robot Finder</h1>
-    </div>
-  );
+	console.log(Robot);
+	return (
+		<div className="App">
+			<h1>Robot Finder</h1>
+		</div>
+	);
 }
 
 export default App;
